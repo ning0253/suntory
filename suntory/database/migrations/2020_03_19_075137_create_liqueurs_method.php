b4703905bb7bfed4e2c04ea5baa2e2a12f8e7e13
ken0911208818 <55379339+ken0911208818@users.noreply.github.com>
2020-03-19 16:21:55 +0800

    資料庫建立

---
+++ suntory/database/migrations/2020_03_19_075137_create_liqueurs_method.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLiqueursMethod extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('liqueurs_method', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('liqueur_id');
+            $table->string('img');
+            $table->string('content');
+            $table->string('title');
+            $table->integer('sort');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('liqueurs_method');
+    }
+}
