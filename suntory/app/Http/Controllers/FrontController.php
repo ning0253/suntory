fa06b5338646d1e62d39981281cbebd8b51c43d9
wangstop <m10612071@gemail.yuntech.edu.tw>
2020-03-29 21:57:31 +0800

    model

--- suntory/app/Http/Controllers/FrontController.php
+++ suntory/app/Http/Controllers/FrontController.php
@@ -2,7 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\LiqueurImg;
+use App\LiqueurSure;
+use App\LiqueurStory;
+use App\LiqueurMethod;
 use App\LiqueurProduct;
+use App\LiqueurAttitude;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -18,15 +23,18 @@ class FrontController extends Controller
 
     public function Yamasaki()
     {
-        $stories = DB::table('liqueur_stroys')->orderBy('sort','desc')->get();
-        $attitudes = DB::table('liqueur_attitudes')->orderBy('sort','desc')->get();
-        $products = DB::table('liqueur_products')->orderBy('sort','desc')->get();
-        $methods = DB::table('liqueur_methods')->orderBy('sort','desc')->get();
-        $sures = DB::table('liqueur_sures')->orderBy('sort','desc')->get();
-        $banners = DB::table('liqueur_imgs')->orderBy('sort','desc')->get();
+
+        $stories = LiqueurStory::orderBy('sort','desc')->get();
+        $attitudes = LiqueurAttitude::orderBy('sort','desc')->get();
+        $products = LiqueurProduct::orderBy('sort','desc')->get();
+        $methods = LiqueurMethod::orderBy('sort','desc')->get();
+        $sures = LiqueurSure::orderBy('sort','desc')->get();
+        $banners = LiqueurImg::orderBy('sort','desc')->get();
 
 
-        // dd($attitudes);
+
+
+        // dd($stories);
 
         return view('Yamasaki', compact('stories','attitudes','products','methods','sures','banners'));
     }
